56d44eeb5abe1fc6facf5fab977256f0ca075921
willy <admin@30b>
2018-11-15 19:46:58 +0700

    version 1.7

--- 165314016_maps/src/java/helper/UserHelper.java
+++ 165314016_maps/src/java/helper/UserHelper.java
@@ -21,38 +21,38 @@ public class UserHelper {
     public UserHelper() {
     }
 
-    public User cariUser(String email) {
-        Session session = NewHibernateUtil.getSessionFactory().openSession();
-        Transaction tx = session.beginTransaction();
-        String query = "from User u where u.email=:email";
-        Query q = session.createQuery(query);
-        q.setParameter("email", email);
-        List<User> list = q.list();
-        tx.commit();
-        session.close();
-        if (list.size() > 0) {
-            return list.get(0);
-        } else {
-            return null;
-        }
-    }
-
-    public User login(String email, String password) {
-        User user = this.cariUser(email);
-        if (user != null) {
-            //user ada 
-            if (user.getPassword().equals(password)) {
-                //password sama
-                return user;
-            } else {
-                //password berbeda
-                return null;
-            }
-        } else {
-            //user tidak ada
-            return null;
-        }
-    }
+//    public User cariUser(String email) {
+//        Session session = NewHibernateUtil.getSessionFactory().openSession();
+//        Transaction tx = session.beginTransaction();
+//        String query = "from User u where u.email=:email";
+//        Query q = session.createQuery(query);
+//        q.setParameter("email", email);
+//        List<User> list = q.list();
+//        tx.commit();
+//        session.close();
+//        if (list.size() > 0) {
+//            return list.get(0);
+//        } else {
+//            return null;
+//        }
+//    }
+//
+//    public User login(String email, String password) {
+//        User user = this.cariUser(email);
+//        if (user != null) {
+//            //user ada 
+//            if (user.getPassword().equals(password)) {
+//                //password sama
+//                return user;
+//            } else {
+//                //password berbeda
+//                return null;
+//            }
+//        } else {
+//            //user tidak ada
+//            return null;
+//        }
+//    }
 
     public List<User> getAllUser() {
         Session session = NewHibernateUtil.getSessionFactory().openSession();
--- 165314016_maps/src/java/services/UserResources.java
+++ 165314016_maps/src/java/services/UserResources.java
@@ -56,32 +56,4 @@ public class UserResources {
     @Consumes(javax.ws.rs.core.MediaType.APPLICATION_JSON)
     public void putJson(String content) {
     }
-    
-    
-    @GET
-    @Path("login")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response Login(
-            @QueryParam("email") String email, 
-            @QueryParam("password") String password){
-        UserHelper helper = new UserHelper();
-        User hasil = helper.login(email, password);
-//        boolean hasil = helper.login(email, password);
-        Gson gson = new Gson();
-        
-         return Response.status(200)
-                .entity(gson.toJson(hasil))
-                .header("Access-Control-Allow-Origin", "*")
-                .header("Access-Control-Allow-Methods",
-                        "GET,POST,HEAD,OPTIONS,PUT")
-                .header("Access-Control-Allow-Headers",
-                        "Content-Type,X-Requested-With,accept,Origin,Access-Control-Request-Method,Access-Control-Request-Headers")
-                .header("Access-Exposed-Headers",
-                        "Access-Control-Allow-Origin,Access-Control-Allow-Credentials")
-                .header("Access-Support-Credentials",
-                        "true")
-                .header("Access-Control-Max-Age","2")
-                .header("Access-Preflight-Maxage", "2")
-                .build();
     }
-}
